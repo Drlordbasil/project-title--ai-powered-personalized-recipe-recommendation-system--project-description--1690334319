56d7862e6f02f6a30396885f2c9cfd750af5096d
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 13:40:49 -0400

    Auto-formatting code

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
 Here are some improvements to the given Python program:
 
 1. Add type annotations: Add type annotations to the function signatures and variable declarations to improve readability and make the code more maintainable.
@@ -19,16 +22,16 @@ Here are some improvements to the given Python program:
 Here's the improved code:
 
 ```python
-import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
 # Constants
-COLUMNS = ['cuisine_types', 'dietary_restrictions', 'cooking_skill', 'favorite_ingredients']
+COLUMNS = ['cuisine_types', 'dietary_restrictions',
+           'cooking_skill', 'favorite_ingredients']
 RECIPE_DATA_FILE = 'recipes.csv'
 NUM_RECOMMENDATIONS = 5
 
 # Load recipe data
+
+
 def load_recipe_data(file_path):
     try:
         return pd.read_csv(file_path)
@@ -36,14 +39,21 @@ def load_recipe_data(file_path):
         print(f"Error: File '{file_path}' not found.")
         return pd.DataFrame()
 
+
 recipes_df = load_recipe_data(RECIPE_DATA_FILE)
 
 # Create user profile
+
+
 def create_profile():
-    cuisine_types = input('Enter your preferred cuisine types (separated by commas): ').split(',')
-    dietary_restrictions = input('Enter your dietary restrictions (separated by commas): ').split(',')
-    cooking_skill = input('Enter your cooking skill level (beginner, intermediate, advanced): ')
-    favorite_ingredients = input('Enter your favorite ingredients (separated by commas): ').split(',')
+    cuisine_types = input(
+        'Enter your preferred cuisine types (separated by commas): ').split(',')
+    dietary_restrictions = input(
+        'Enter your dietary restrictions (separated by commas): ').split(',')
+    cooking_skill = input(
+        'Enter your cooking skill level (beginner, intermediate, advanced): ')
+    favorite_ingredients = input(
+        'Enter your favorite ingredients (separated by commas): ').split(',')
 
     return {
         'cuisine_types': cuisine_types,
@@ -52,20 +62,26 @@ def create_profile():
         'favorite_ingredients': favorite_ingredients
     }
 
+
 user_profile = create_profile()
 
 # Filter recipes based on user preferences
+
+
 def filter_recipes(recipes_df, user_profile):
     filtered_recipes_df = recipes_df.copy()
 
     # Filter based on cuisine types
-    filtered_recipes_df = filtered_recipes_df[filtered_recipes_df[COLUMNS[0]].isin(user_profile['cuisine_types'])]
+    filtered_recipes_df = filtered_recipes_df[filtered_recipes_df[COLUMNS[0]].isin(
+        user_profile['cuisine_types'])]
 
     # Filter based on dietary restrictions
-    filtered_recipes_df = filtered_recipes_df[~filtered_recipes_df[COLUMNS[1]].isin(user_profile['dietary_restrictions'])]
+    filtered_recipes_df = filtered_recipes_df[~filtered_recipes_df[COLUMNS[1]].isin(
+        user_profile['dietary_restrictions'])]
 
     # Filter based on cooking skill
-    filtered_recipes_df = filtered_recipes_df[filtered_recipes_df[COLUMNS[2]] == user_profile['cooking_skill']]
+    filtered_recipes_df = filtered_recipes_df[filtered_recipes_df[COLUMNS[2]]
+                                              == user_profile['cooking_skill']]
 
     # Filter based on favorite ingredients
     filtered_recipes_df['ingredient_count'] = [sum(1 for ingredient in ingredients.split(',') if ingredient.strip() in user_profile['favorite_ingredients'])
@@ -74,24 +90,34 @@ def filter_recipes(recipes_df, user_profile):
 
     return filtered_recipes_df
 
+
 filtered_recipes_df = filter_recipes(recipes_df, user_profile)
 
 # Calculate recipe similarities
+
+
 def calculate_similarity(filtered_recipes_df):
     count_vectorizer = CountVectorizer(stop_words='english')
-    count_matrix = count_vectorizer.fit_transform(filtered_recipes_df['ingredients'].values.astype('U'))
+    count_matrix = count_vectorizer.fit_transform(
+        filtered_recipes_df['ingredients'].values.astype('U'))
     similarity_matrix = cosine_similarity(count_matrix, count_matrix)
 
     return similarity_matrix
 
+
 similarity_matrix = calculate_similarity(filtered_recipes_df)
 
 # Get top recommendations
+
+
 def get_recommendations():
-    recipe_indices = similarity_matrix[-1].argsort()[::-1][1:NUM_RECOMMENDATIONS + 1]
-    recommendations = filtered_recipes_df.iloc[recipe_indices]['recipe_name'].tolist()
+    recipe_indices = similarity_matrix[-1].argsort()[::-
+                                                     1][1:NUM_RECOMMENDATIONS + 1]
+    recommendations = filtered_recipes_df.iloc[recipe_indices]['recipe_name'].tolist(
+    )
 
     return recommendations
+
 
 # Get recipe recommendations
 recommendations = get_recommendations()
